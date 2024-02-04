0701cadf1a947967da20592fda1aa719533d5456
insshubh <insshubh22@gmail.com>
2024-02-04 13:27:07 +0530

    homepage-done

--- MultiUserCodingPlatform/src/Pages/HomePage.jsx
+++ MultiUserCodingPlatform/src/Pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
+
 function HomePage() {
   return (
     <>
+    
       <section className="flex flex-col lg:flex-row h-screen bg-black bg-opacity-99">
         <div className="lg:w-2/3">
           <div className="relative h-[45vh] lg:h-full">
@@ -16,19 +18,26 @@ function HomePage() {
             <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"></div>
             <div className="relative flex flex-col items-center justify-center">
               <div className="w-full max-w-xl xl:mx-auto xl:w-full xl:max-w-xl">
-                <h2 className="text-3xl font-bold text-white opacity-70 text-center">
-                  Collaborative Code Symphony: Unleash Innovation Together.
-                </h2>
+              <div className="relative">
+  <h2 className="img-text text-3xl font-bold text-center relative z-10 group">
+    Collaborative Code Symphony: Unleash Innovation Together.
+  </h2>
+</div>
+
+
+
+
               </div>
             </div>
           </div>
         </div>
 
-        <div class="w-full lg:w-1/2 flex flex-col items-center justify-center">
+        <div className="w-full lg:w-1/2 flex flex-col items-center justify-center">
           <form className="border border-gray-600 py-1 px-2 border-radius-2  text-white w-full sm:w-1/2 h-auto rounded-[20px]">
-            <p class="mb-4 p-2">Please login to your account</p>
+          <p className="mb-4 p-2 text-xl font-bold text-white">Please login to your account</p>
 
-            <div class="relative mb-4">
+
+            <div className="relative mb-4">
               <input
                 type="text"
                 className="py-3 peer block w-full rounded-md border-none bg-white px-3 py-2 text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-75 transition-all duration-200 ease-linear focus:placeholder-gray-500"
@@ -37,7 +46,7 @@ function HomePage() {
               />
             </div>
 
-            <div class="relative mb-4">
+            <div className="relative mb-4">
               <input
                 type="password"
                 className="py-3 peer block w-full rounded-md border-none bg-white px-3 py-2 text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-75 transition-all duration-200 ease-linear focus:placeholder-gray-500"
@@ -46,30 +55,32 @@ function HomePage() {
               />
             </div>
 
-            <div class="mb-4 flex items-center justify-between">
+            <div className="mb-4 flex items-center justify-between">
               <button
                 type="button"
-                class="w-full bg-gradient-to-r from-amber-400 to-red-500 text-white py-2 px-4 rounded-md font-medium uppercase tracking-wide hover:bg-gradient-to-r from-red-400 to-pink-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 focus:ring-opacity-75 transition-all duration-200 ease-linear"
+                className="w-full bg-gradient-to-r from-amber-400 to-red-500 text-white py-2 px-4 rounded-md font-medium uppercase tracking-wide hover:bg-gradient-to-r from-red-400 to-pink-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 focus:ring-opacity-75 transition-all duration-200 ease-linear"
               >
                 JOIN{" ->"}
               </button>
             </div>
 
-            <div class="flex flex-col items-center justify-between pb-6">
-              <p class="mb-0 mr-2 text-white">Don't have a Room ID?</p>
+            <div className="flex flex-col items-center justify-between pb-6">
+              <p className="mb-0 mr-2 text-white">Don't have a Room ID?</p>
               <button
                 type="button"
-                class="my-2 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 focus:ring-opacity-75 transition-all duration-200 ease-linear"
+                className="my-2 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 focus:ring-opacity-75 transition-all duration-200 ease-linear"
               >
                 Create New ID
               </button>
             </div>
             <footer className="text-white items-center p-4 fixed bottom-0 right-0">
-              <div className="relative">
-                <p className="text-s font-bold tracking-wide bg-gradient-to-l from-amber-100 to-red-400 bg-clip-text text-transparent">
-                  Made By GodZ
-                </p>
-              </div>
+             
+            <div className="relative group">
+  <p className="text-s font-bold tracking-wide bg-gradient-to-l from-amber-100 to-red-400 bg-clip-text text-transparent group-hover:from-white">
+    Made By <a className="text-s font-bold tracking-wide bg-gradient-to-l from-amber-100 to-red-400 bg-clip-text text-transparent group-hover:from-white group-hover:to-white group-hover:after:from-rose-300 group-hover:after:to-fuchsia-700" href="https://github.com/insshubh">GodZ</a>
+  </p>
+</div>
+
             </footer>
           </form>
         </div>
