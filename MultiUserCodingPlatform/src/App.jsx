f8128ca271e2782aa608335ed076701b5dff7131
insshubh <insshubh22@gmail.com>
2024-02-26 20:31:00 +0530

    Added Button Listening

--- MultiUserCodingPlatform/src/App.jsx
+++ MultiUserCodingPlatform/src/App.jsx
@@ -1,22 +1,25 @@
 import { useState } from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './Pages/HomePage'
 import EditorPage from './Pages/Editorpage'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <BrowserRouter>
-     <Routes>
-     <Route path='/' element={<HomePage/>}></Route>
-     <Route path='/editor/:roomId' element={<EditorPage/>}></Route>
-     </Routes>
-     </BrowserRouter>
+      <div>
+        <Toaster position="top-center" />
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/editor/:roomId' element={<EditorPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
 export default App
+
--- MultiUserCodingPlatform/src/Pages/HomePage.jsx
+++ MultiUserCodingPlatform/src/Pages/HomePage.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { v4 as uid }  from "uuid";
+import toast from "react-hot-toast";
+import { Navigation } from "react-router";
 
 function HomePage() {
+  // set the username and room id with help uuid module
+    const [roomID, setId] = useState('');
+    const [user, setuser] = useState('');
+
+   const newroom = (e) => {
+    const id = uid();
+    setId(id);
+    toast.success("ID Created Succesfully")
+   }
+   const joinroom =()=>{
+     if(!user && !roomID)
+     {
+      toast.error("Please Enter Username and Room ID")
+     }
+
+     // if It is Valid then Redirect to
+   }
+
   return (
     <>
-    
       <section className="flex flex-col lg:flex-row h-screen bg-black bg-opacity-99">
         <div className="lg:w-2/3">
           <div className="relative h-[45vh] lg:h-full">
@@ -19,15 +38,11 @@ function HomePage() {
             <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"></div>
             <div className="relative flex flex-col items-center justify-center">
               <div className="w-full max-w-xl xl:mx-auto xl:w-full xl:max-w-xl">
-              <div className="relative">
-  <h2 className="img-text text-3xl font-bold text-center relative z-10 group">
-    Collaborative Code Symphony: Unleash Innovation Together.
-  </h2>
-</div>
-
-
-
-
+                <div className="relative">
+                  <h2 className="img-text text-3xl font-bold text-center relative z-10 group">
+                    Collaborative Code Symphony: Unleash Innovation Together.
+                  </h2>
+                </div>
               </div>
             </div>
           </div>
@@ -35,8 +50,9 @@ function HomePage() {
 
         <div className="w-full lg:w-1/2 flex flex-col items-center justify-center">
           <form className="border border-gray-600 py-1 px-2 border-radius-2  text-white w-full sm:w-1/2 h-auto rounded-[20px]">
-          <p className="mb-4 p-2 text-xl font-bold text-white">Please login to your account</p>
-
+            <p className="mb-4 p-2 text-xl font-bold text-white">
+              Please login to your account
+            </p>
 
             <div className="relative mb-4">
               <input
@@ -44,15 +60,20 @@ function HomePage() {
                 className="py-3 peer block w-full rounded-md border-none bg-white px-3 py-2 text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-75 transition-all duration-200 ease-linear focus:placeholder-gray-500"
                 id="exampleFormControlInput1"
                 placeholder="Username"
+                onChange={(e) => setuser(e.target.value)}
+                value={user}
               />
             </div>
 
             <div className="relative mb-4">
               <input
-                type="password"
+                type="id"
                 className="py-3 peer block w-full rounded-md border-none bg-white px-3 py-2 text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-75 transition-all duration-200 ease-linear focus:placeholder-gray-500"
                 id="exampleFormControlInput11"
                 placeholder="Room ID"
+                onChange={(e) => setId(e.target.value)}
+                value={roomID}
+                
               />
             </div>
 
@@ -60,6 +81,7 @@ function HomePage() {
               <button
                 type="button"
                 className="w-full bg-gradient-to-r from-amber-400 to-red-500 text-white py-2 px-4 rounded-md font-medium uppercase tracking-wide hover:bg-gradient-to-r from-red-400 to-pink-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 focus:ring-opacity-75 transition-all duration-200 ease-linear"
+                onClick={joinroom}
               >
                 JOIN{" ->"}
               </button>
@@ -67,7 +89,7 @@ function HomePage() {
 
             <div className="flex flex-col items-center justify-between pb-6">
               <p className="mb-0 mr-2 text-white">Don't have a Room ID?</p>
-              <button
+              <button onClick={newroom}
                 type="button"
                 className="my-2 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 focus:ring-opacity-75 transition-all duration-200 ease-linear"
               >
@@ -75,16 +97,11 @@ function HomePage() {
               </button>
             </div>
             <footer className="text-white items-center p-4 fixed bottom-0 right-0">
-             
-            <div className="relative group">
-            <p className="text-s font-bold tracking-wide bg-gradient-to-l from-amber-100 to-red-400 bg-clip-text text-transparent group-hover:from-white">
-  <Link to="https://github.com/insshubh">
-  Made By GodZ
-  </Link>
-</p>
-
-</div>
-
+              <div className="relative group">
+                <p className="text-s font-bold tracking-wide bg-gradient-to-l from-amber-100 to-red-400 bg-clip-text text-transparent group-hover:from-white">
+                  <Link to="https://github.com/insshubh">Made By GodZ</Link>
+                </p>
+              </div>
             </footer>
           </form>
         </div>
@@ -94,7 +111,3 @@ function HomePage() {
 }
 
 export default HomePage;
-
-""
-
-
